323aaba6a28b53bf58ed1dd7defb89664c2182f6
Milos <milose@users.noreply.github.com>
2017-01-14 03:47:02 +0100

    Add JavaScript standard. (#1)
    * Add JavaScript standard.
    
    * Add eslint and hound configuration.
    
    * Update the name of eslint json file.
    
    * Add error handling.
    
    * Modify hound and eslint options.

--- index.js
+++ index.js
@@ -1,84 +1,86 @@
 #!/usr/bin/env node
 
-'use strict';
+'use strict'
+
+const path = require('path')
 
 require('dotenv').config({
-    path: __dirname + '/.env'
-});
-var channel = process.env.WS_KEY + '.' + process.env.WS_USER;
-var toEprom = process.env.RDS_WRITE == 'true';
+  path: path.join(__dirname, '.env')
+})
+var channel = process.env.WS_KEY + '.' + process.env.WS_USER
+var toEprom = process.env.RDS_WRITE === 'true'
 
-var request = require('request');
-var serial = require('./modules/serial-rds');
-var format = require('./modules/serial-format');
+var request = require('request')
+var serial = require('./modules/serial-rds')
+var format = require('./modules/serial-format')
 var socket = require('socket.io-client')(process.env.WS_URI + ':' + process.env.WS_PORT, {
-    timeout: false,
-    query: "username=" + process.env.WS_USER,
-});
+  timeout: false,
+  query: 'username=' + process.env.WS_USER
+})
 
 // Main events
-socket.on('connect', function() {
-    var msg = 'Connected to ' + socket.io.uri + ' ' + channel;
+socket.on('connect', function () {
+  var msg = 'Connected to ' + socket.io.uri + ' ' + channel
 
-    if (process.env.RDS_SILENT == 'false') {
-        console.log(msg);
-    }
+  if (process.env.RDS_SILENT === 'false') {
+    console.log(msg)
+  }
 
     // notify(msg);
-});
+})
 
-socket.on('disconnect', function() {
-    var msg = 'Disconnected. Writing to RDS: ' + process.env.RDS_DEFAULT;
+socket.on('disconnect', function () {
+  var msg = 'Disconnected. Writing to RDS: ' + process.env.RDS_DEFAULT
 
-    if (process.env.RDS_SILENT == 'false') {
-        console.log(msg);
-    }
+  if (process.env.RDS_SILENT === 'false') {
+    console.log(msg)
+  }
 
-    serial.send(format.rdsPrepare(process.env.RDS_DEFAULT), toEprom, process.env.RDS_PORT, process.env.RDS_RATE, process.env.RDS_PS, write_error);
+  serial.send(format.rdsPrepare(process.env.RDS_DEFAULT), toEprom, process.env.RDS_PORT, process.env.RDS_RATE, process.env.RDS_PS, writeError)
 
-    notify(msg);
-});
+  notify(msg)
+})
 
-socket.on(channel, function(data) {
-    if (data == null) return;
+socket.on(channel, function (data) {
+  if (data == null) return
 
-    var msg = 'Writing to RDS: ' + data;
+  var msg = 'Writing to RDS: ' + data
 
-    if (process.env.RDS_SILENT == 'false') {
-        console.log(msg);
-    }
+  if (process.env.RDS_SILENT === 'false') {
+    console.log(msg)
+  }
 
-    serial.send(data, toEprom, process.env.RDS_PORT, process.env.RDS_RATE, process.env.RDS_PS, write_error);
-});
+  serial.send(data, toEprom, process.env.RDS_PORT, process.env.RDS_RATE, process.env.RDS_PS, writeError)
+})
 
-var write_error = function(error) {
-    var msg = 'Serial error: ' + error;
+var writeError = function (error) {
+  var msg = 'Serial error: ' + error
 
-    if (process.env.RDS_SILENT == 'false') {
-        console.log(msg);
-    }
+  if (process.env.RDS_SILENT === 'false') {
+    console.log(msg)
+  }
 
-    notify(msg);
+  notify(msg)
 }
 
-function notify(text) {
-    if (process.env.SLACK_URL == '') return;
+function notify (text) {
+  if (process.env.SLACK_URL === '') return
 
-    try {
-        request.post(process.env.SLACK_URL, {
-            form: {
-                payload: JSON.stringify({
-                    'username': process.env.WS_USER,
-                    'icon_emoji': process.env.SLACK_ICON,
-                    'text': text,
-                })
-            }
-        });
-    } catch (ex) {
-        var msg = 'Slack error: ' + ex;
+  try {
+    request.post(process.env.SLACK_URL, {
+      form: {
+        payload: JSON.stringify({
+          'username': process.env.WS_USER,
+          'icon_emoji': process.env.SLACK_ICON,
+          'text': text
+        })
+      }
+    })
+  } catch (ex) {
+    var msg = 'Slack error: ' + ex
 
-        if (process.env.RDS_SILENT == 'false') {
-            console.log(msg);
-        }
+    if (process.env.RDS_SILENT === 'false') {
+      console.log(msg)
     }
+  }
 }
