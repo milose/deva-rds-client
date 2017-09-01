a7438bce2115cd07eacff7a506bcb182ed0fbe28
Milos Sakovic <m@deva.co>
2017-09-01 18:06:59 +0200

    Arrow function should not return assignment.

--- modules/txt-reader.js
+++ modules/txt-reader.js
@@ -5,7 +5,9 @@ const http = require('http')
 exports.get = (host, path, callback) => {
   return http.get({ host, path }, response => {
     let body = ''
-    response.on('data', d => body += d)
+    response.on('data', d => {
+      body += d
+    })
     response.on('end', () => callback(body))
   })
 }
