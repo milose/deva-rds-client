c2d2482cfac3ce856b22decbdeed5c530c6978ec
Milos Sakovic <m@deva.co>
2017-09-01 21:12:06 +0200

    Extract clean to a separate module.

---
+++ modules/clean.js
@@ -0,0 +1,32 @@
+'use strict'
+
+const diacritics = require('diacritics')
+
+exports = input => {
+  input = diacritics.remove(input)
+
+  return input.trim()
+    .replace('\r', ' ')
+    .replace('\n', ' ')
+    .replace('!', '')
+    .replace('@', '')
+    .replace('#', '')
+    .replace('%', '')
+    .replace('^', '')
+    .replace('&', '')
+    .replace('*', '')
+    .replace('=', '')
+    .replace('+', '')
+    .replace('?', '')
+    .replace(',', '.')
+    .replace('$', 'S')
+    .replace('"', '\'')
+    .replace(';', '').replace(':', '')
+    .replace('-', '').replace('_', '')
+    .replace('<', '').replace('>', '')
+    .replace('{', '').replace('}', '')
+    .replace('[', '').replace(']', '')
+    .replace('(', '').replace(')', '')
+    .replace('/', '').replace('\\', '')
+    .replace('  ', ' ')
+}
--- modules/serial-format.js
+++ modules/serial-format.js
@@ -1,9 +1,18 @@
 'use strict'
 const pad = require('./pad')
-const diacritics = require('diacritics')
+const clean = require('./clean')
 
 const max = 8
 const maxDps = 80
+
+/*
+    Commands
+ */
+exports.cmdRead = () => hexBuffer('fed000c0ff')
+
+exports.cmdEprom = () => hexBuffer('fe7145ff')
+
+exports.cmdReboot = () => hexBuffer('fe7152ff')
 
 /*
     RDS formatters
@@ -12,12 +21,11 @@ exports.dynamic = input => {
   input = this.rdsPrepare(input)
   if (input.length > maxDps) input = input.substring(0, maxDps)
 
-  // @TODO: 20fffe76 vs fffe76
   return hexBuffer('fe7600' + hexString(input) + 'fffe76' + hexCount(input) + 'ff')
 }
 
 exports.ps = input => {
-  input = this.cleanUp(input)
+  input = clean(input)
   if (input.length > max) input = input.substring(0, max)
   if (input.length < max) input = pad.right(input, max)
 
@@ -25,7 +33,7 @@ exports.ps = input => {
 }
 
 exports.psBuffered = input => {
-  input = this.cleanUp(input)
+  input = clean(input)
   if (input.length > max) input = input.substring(0, max)
   if (input.length < max) input = pad.right(input, max)
 
@@ -33,33 +41,21 @@ exports.psBuffered = input => {
 }
 
 /*
-    Commands
- */
-exports.cmdRead = () => {
-  return hexBuffer('fed000c0ff')
-}
-
-exports.cmdEprom = () => {
-  return hexBuffer('fe7145ff')
-}
-
-exports.cmdReboot = () => {
-  return hexBuffer('fe7152ff')
-}
-
-/*
     Helpers
  */
 exports.rdsPrepare = string => {
   // make an array and remove empty items
-  return this.cleanUp(string).split(' ').filter((word) => word.trim() !== '')
-    // make an array of words that are exactly max wide
-    .map((word) => {
+  return clean(string)
+    .split(' ')
+    .filter(word => word.trim() !== '')
+    .map((word) => { // make an array of words that are exactly max wide
       const times = Math.ceil(word.length / max)
+
       if (times > 1) {
         // word longer then max
         const diff = word.length - max
         let split = []
+
         for (let i = 0; i <= diff; i++) {
           split.push(word.substring(i, max + i))
         }
@@ -71,35 +67,6 @@ exports.rdsPrepare = string => {
       }
     })
     .join('')
-}
-
-exports.cleanUp = string => {
-  string = diacritics.remove(string)
-
-  return string.trim()
-    .replace('\r', ' ')
-    .replace('\n', ' ')
-    .replace('!', '')
-    .replace('@', '')
-    .replace('#', '')
-    .replace('%', '')
-    .replace('^', '')
-    .replace('&', '')
-    .replace('*', '')
-    .replace('=', '')
-    .replace('+', '')
-    .replace('?', '')
-    .replace(',', '.')
-    .replace('$', 'S')
-    .replace('"', '\'')
-    .replace(';', '').replace(':', '')
-    .replace('-', '').replace('_', '')
-    .replace('<', '').replace('>', '')
-    .replace('{', '').replace('}', '')
-    .replace('[', '').replace(']', '')
-    .replace('(', '').replace(')', '')
-    .replace('/', '').replace('\\', '')
-    .replace('  ', ' ')
 }
 
 /*
