853b072b53440054f46f4a5279eb7ea85630e2e7
Milos Sakovic <m@deva.co>
2017-09-01 17:20:17 +0200

    Use diacritics to clean up incoming text.

--- modules/serial-format.js
+++ modules/serial-format.js
@@ -1,5 +1,5 @@
 'use strict'
-
+const diacritics = require('diacritics')
 let max = 8
 let maxDps = 80
 
@@ -10,7 +10,7 @@ exports.dynamic = function (input) {
   input = this.rdsPrepare(input)
   if (input.length > maxDps) input = input.substring(0, maxDps)
 
-    // @TODO: 20fffe76 vs fffe76
+  // @TODO: 20fffe76 vs fffe76
   return hexBuffer('fe7600' + hexString(input) + 'fffe76' + hexCount(input) + 'ff')
 }
 
@@ -49,55 +49,55 @@ exports.cmdReboot = function () {
     Helpers
  */
 exports.rdsPrepare = function (string) {
-    // make an array and remove empty items
+  // make an array and remove empty items
   return this.cleanUp(string).split(' ').filter((word) => word.trim() !== '')
-        // make an array of words that are exactly max wide
-        .map((word) => {
-          let times = Math.ceil(word.length / max)
-          if (times > 1) {
-                // word longer then max
-            let diff = word.length - max
-            let split = []
-            for (let i = 0; i <= diff; i++) {
-              split.push(word.substring(i, max + i))
-            }
-                // return as one word
-            return split.join('')
-          } else {
-                // word shorter then max, center it.
-            return word.pad(max)
-          }
-        })
-        .join('')
+    // make an array of words that are exactly max wide
+    .map((word) => {
+      let times = Math.ceil(word.length / max)
+      if (times > 1) {
+        // word longer then max
+        let diff = word.length - max
+        let split = []
+        for (let i = 0; i <= diff; i++) {
+          split.push(word.substring(i, max + i))
+        }
+        // return as one word
+        return split.join('')
+      } else {
+        // word shorter then max, center it.
+        return word.pad(max)
+      }
+    })
+    .join('')
 }
 
 exports.cleanUp = function (string) {
+  string = diacritics.remove(string)
+
   return string.trim()
-        // clean unwanted characters
-        .latinise()
-        .replace('\r', ' ')
-        .replace('\n', ' ')
-        .replace('!', '')
-        .replace('@', '')
-        .replace('#', '')
-        .replace('%', '')
-        .replace('^', '')
-        .replace('&', '')
-        .replace('*', '')
-        .replace('=', '')
-        .replace('+', '')
-        .replace('?', '')
-        .replace(',', '.')
-        .replace('$', 'S')
-        .replace('"', '\'')
-        .replace(';', '').replace(':', '')
-        .replace('-', '').replace('_', '')
-        .replace('<', '').replace('>', '')
-        .replace('{', '').replace('}', '')
-        .replace('[', '').replace(']', '')
-        .replace('(', '').replace(')', '')
-        .replace('/', '').replace('\\', '')
-        .replace('  ', ' ')
+    .replace('\r', ' ')
+    .replace('\n', ' ')
+    .replace('!', '')
+    .replace('@', '')
+    .replace('#', '')
+    .replace('%', '')
+    .replace('^', '')
+    .replace('&', '')
+    .replace('*', '')
+    .replace('=', '')
+    .replace('+', '')
+    .replace('?', '')
+    .replace(',', '.')
+    .replace('$', 'S')
+    .replace('"', '\'')
+    .replace(';', '').replace(':', '')
+    .replace('-', '').replace('_', '')
+    .replace('<', '').replace('>', '')
+    .replace('{', '').replace('}', '')
+    .replace('[', '').replace(']', '')
+    .replace('(', '').replace(')', '')
+    .replace('/', '').replace('\\', '')
+    .replace('  ', ' ')
 }
 
 /*
@@ -106,7 +106,7 @@ exports.cleanUp = function (string) {
 let hexString = function (string) {
   let hex = new Buffer(string, 'ascii').toString('hex')
 
-    // Byte values 0xFD, 0xFE, and 0xFF are transformed into a pair of bytes
+  // Byte values 0xFD, 0xFE, and 0xFF are transformed into a pair of bytes
   return hex.replace('fd', 'fd00').replace('fe', 'fd01').replace('ff', 'fd02')
 }
 
