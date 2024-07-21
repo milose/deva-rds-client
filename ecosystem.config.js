7f8348e1f40b1846300d98ca6ce3c79318db8e8b
milose <milos@deva.co>
2024-07-21 16:09:16 +0200

    Add pm2 ecosystem.

---
+++ ecosystem.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    apps: [
+        {
+            name: 'radio-rds-client',
+            script: './app.js',
+            watch: true,
+            ignore_watch: ['node_modules'],
+        },
+    ],
+}
