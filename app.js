4fd4b95ccdae321182f4ee6de289d8ac5d8fd808
City <city@rds.drugacija.me>
2024-07-21 21:02:29 +0200

    Fix serial error.

--- app.js
+++ app.js
@@ -11,66 +11,70 @@ const channel = env.WS_KEY + '.' + env.WS_USER
 const baudRate = parseInt(env.RDS_RATE)
 
 const socket = socketio(env.WS_URI, {
-    timeout: false,
-    query: 'username=' + env.WS_USER,
+  timeout: false,
+  query: 'username=' + env.WS_USER,
 })
 
 // Main events
 socket.on('connect', () => log(`Connected to ${socket.io.uri} ${channel}`))
 
 socket.on(channel, (data) => {
-    if (data == null) return
+  if (data == null) return
 
-    log(`Received: ${data} => ${clean(data)}`)
+  if (data === 'reboot') {
+    return serial.reboot(env.RDS_PORT, baudRate, serialError)
+  }
 
-    serial.send(
-        data,
-        env.RDS_WRITE === 'true',
-        env.RDS_PORT,
-        baudRate,
-        env.RDS_PS,
-        serialError
-    )
+  log(`Received: ${data} => ${clean(data)}`)
+
+  serial.send(
+    data,
+    env.RDS_WRITE === 'true',
+    env.RDS_PORT,
+    baudRate,
+    env.RDS_PS,
+    serialError
+  )
 })
 
 socket.on('disconnect', () => {
-    const msg = `Disconnected. Writing to RDS: ${env.RDS_DEFAULT}`
-    log(msg)
-    if (process.env.NODE_ENV !== 'production') notify(msg)
+  const msg = `Disconnected. Writing to RDS: ${env.RDS_DEFAULT}`
+  log(msg)
+  if (process.env.NODE_ENV !== 'production') notify(msg)
 
-    serial.send(
-        env.RDS_DEFAULT,
-        env.RDS_WRITE === 'true',
-        env.RDS_PORT,
-        baudRate,
-        env.RDS_PS,
-        serialError
-    )
+  serial.send(
+    env.RDS_DEFAULT,
+    env.RDS_WRITE === 'true',
+    env.RDS_PORT,
+    baudRate,
+    env.RDS_PS,
+    serialError
+  )
 })
 
 const serialError = (error, data) => {
-    const msg = `Serial error: ${error}`
-    notify(msg, data)
-    log(msg, data)
+  const msg = `Serial error: ${error}`
+  notify(msg + ' (' + data + ')')
+  log(msg, data)
 }
 
-const notify = async (content, extras = null) => {
-    if (!env.SLACK_URL) return
+const notify = async (content) => {
+  if (!env.SLACK_URL) return
 
-    let response = await fetch(env.SLACK_URL, {
-        method: 'post',
-        body: JSON.stringify({
-            content: content + ' ' + extras,
-            username: env.WS_USER,
-            avatar_url: env.SLACK_AVATAR ?? null,
-        }),
-        headers: { 'Content-Type': 'application/json' },
-    })
-    if (!response.ok) {
-        log(`Slack error: ${response.status}`)
-    }
+  let response = await fetch(env.SLACK_URL, {
+    method: 'post',
+    body: JSON.stringify({
+      content,
+      username: env.WS_USER,
+      avatar_url: env.SLACK_AVATAR ?? null,
+    }),
+    headers: { 'Content-Type': 'application/json' },
+  })
+  if (!response.ok) {
+    log(`Slack error: ${response.status}`)
+  }
 }
 
 const log = (message, data = '') => {
-    if (process.env.NODE_ENV !== 'production') console.log(message, data)
+  if (process.env.NODE_ENV !== 'production') console.log(message, data)
 }
